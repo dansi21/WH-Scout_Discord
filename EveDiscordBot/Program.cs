20f9a5e2f6c16ef3c7c3a9c70122608e49a2b058
William Lathrop <willguy21@gmail.com>
2021-10-03 09:30:16 -0700

    Add project files.

---
+++ EveDiscordBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using System.IO;
+
+namespace EveDiscordBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+
+            var token = File.ReadAllText("token.txt");
+
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage logMessage) {
+            Console.WriteLine(logMessage.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
